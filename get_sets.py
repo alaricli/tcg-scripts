69f375fc943139d8f013aaee2fbb2c0ba51734bd
alaricli <alaricli@outlook.com>
2024-11-26 21:12:42 -0800

    modify scripts

--- get_sets.py
+++ get_sets.py
@@ -5,7 +5,7 @@ RestClient.configure('e04b950b-6e91-4c21-b820-06fd135d9b8a')
 def get_all_sets():
   sets = Set.all()
   for s in sets:
-    print(f"Set Name: {s.name}, Series: {s.series}, Id: {s.id}, ptcgoCode: {s.ptcgoCode}, total: {s.total}")
+    print(f"{s.ptcgoCode}")
 
 if __name__ == "__main__":
     print("Fetching all sets...")
