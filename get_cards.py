1687e9819bcf0634c29f5aa1fd906cc28f0101c5
Alaric Li <alaricli@outlook.com>
2024-11-29 11:56:32 -0800

    refactor json saving

--- get_cards.py
+++ get_cards.py
@@ -3,20 +3,24 @@ import json
 
 RestClient.configure('e04b950b-6e91-4c21-b820-06fd135d9b8a')
 
+# todo: sort data into my format before putting into list
+# todo: save the list
+
 def get_cards_by_set(set_name):
   print(f"Fetching cards from set: {set_name}")
-  query = f'set.name:"{set_name}"'
+  query = f'set.name:"{set_name}" supertype:trainer subtypes:item rarity:rare'
   cards = Card.where(q=query)
   return cards
 
 if __name__ == "__main__":
     set_name = input("Enter the set name: ")
     cards = get_cards_by_set(set_name)
-
     cards_json = []
 
     for card in cards:
-      card_json = json.dumps(card, default=lambda o: o.__dict__, indent=4)
-      cards_json.append(card_json)
-    
-    print(cards_json)
+       card_json = json.dumps(card, default=lambda o: o.__dict__, indent=4)
+       cards_json.append(json.loads(card_json))
+      
+    output = json.dumps(cards_json, indent=4)
+
+    print(output)
